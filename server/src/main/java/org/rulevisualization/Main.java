b1bfddf0a636e2032e6b6a0ee3eb33ea5fc40a5b
mateuszl1995 <mateusz.p.lewandowski@student.put.poznan.pl>
2019-10-15 16:18:50 +0200

    Remove static Http handler (it was unused).

--- server/src/main/java/org/rulevisualization/Main.java
+++ server/src/main/java/org/rulevisualization/Main.java
@@ -1,11 +1,7 @@
 package org.rulevisualization;
 
-import java.io.File;
 import java.net.URI;
-import java.net.URISyntaxException;
-
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -18,17 +14,9 @@ public class Main {
         rc.register(CorsResponseFilter.class);
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
-    
-    public static void addStaticFiles(HttpServer server) throws URISyntaxException {
-    	File dir = new File(ClassLoader.getSystemClassLoader().getResource(".").getPath());
-        File resources = new File(dir, "data");
-        StaticHttpHandler staticHttpHandler = new StaticHttpHandler(resources.getPath());
-        server.getServerConfiguration().addHttpHandler(staticHttpHandler, "/data/");
-    }
 
     public static void main(String[] args) throws Exception {
         final HttpServer server = startServer();
-        addStaticFiles(server);
         
         System.out.println(String.format("Restful service for parsing rules input files started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
